6e1a0bc39f2a96076f387af9761e5060ff82aa2c
mujaddid <mujaddidihsan298@gmail.com>
2025-03-14 08:37:55 +0700

    web: nak

---
+++ resources/views/pages/from_nsk.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+  <!-- [ breadcrumb ] start -->
+  <div class="page-header">
+    <div class="page-block">
+      <div class="row align-items-center">
+        <div class="col-md-12">
+          <div class="page-header-title">
+            <h5 class="m-b-10">Home</h5>
+          </div>
+          <ul class="breadcrumb"> 
+            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+            {{-- <li class="breadcrumb-item" aria-current="page">Dashboard</li> --}}
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- [ breadcrumb ] end -->
+
+  <!-- [ Main Content ] start -->
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-header">
+          <h5>Welcome to the Home Page</h5>
+        </div>
+        <div class="card-body">
+          <p>Your content goes here.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- [ Main Content ] end -->
+@endsection
