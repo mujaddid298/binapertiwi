50075c288e0e99dfd87a0a4704820f7eb067d922
Your Name <maisarasara751@.com>
2025-03-17 10:29:06 +0700

    Jadwal Meeting

--- resources/views/pages/meeting.blade.php
+++ resources/views/pages/meeting.blade.php
@@ -1,22 +1,48 @@
 @extends('layouts.app')
 
+@section('title', 'Jadwal Meeting')
+
 @section('content')
-<h1>Jadwal Meeting</h1>
+<!-- [ breadcrumb ] start -->
+<div class="page-header">
+    <div class="page-block">
+        <div class="row align-items-center">
+            <div class="col-md-12">
+                <div class="page-header-title">
+                    <h5 class="m-b-10">Jadwal Meeting</h5>
+                </div>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Jadwal Meeting</a></li>
+                    {{-- <li class="breadcrumb-item" aria-current="page">Dashboard</li> --}}
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- [ breadcrumb ] end -->
 
-<a href="#">Buat Meeting Baru</a>
+<!-- [ Main Content ] start -->
+<div class="col-md-12 col-xl-12">
+    <div class="card tbl-card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover table-borderless mb-0">
+                    <thead>
+                        <tr>
+                            <th>Judul</th>
+                            <th>Tanggal</th>
+                            <th>Deskripsi</th>
+                            <th>Status</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-<table>
-    <thead>
-        <tr>
-            <th>Judul</th>
-            <th>Tanggal</th>
-            <th>Deskripsi</th>
-            <th>Status</th>
-            <th>Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-
-    </tbody>
-</table>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- [ Main Content ] end -->
 @endsection
